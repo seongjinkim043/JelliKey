4d0fd86426169020dd1f13b993bc6c99d0c1a299
seongjin79 <skssk5656@nanugo.kr>
2025-08-30 12:18:12 +0900

    project update

---
+++ src/main/java/com/jk/jellikey/JelliKeyApplication.java
@@ -0,0 +1,13 @@
+package com.jk.jellikey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JelliKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JelliKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/jellikey/JelliKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.jellikey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JelliKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
